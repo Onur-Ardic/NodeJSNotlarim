ed06134ae5863d8cd0d0b74fd35b50b6414b4f01
Onur Ardıç <onurardc@outlook.com>
2024-08-27 16:09:19 +0300

    readme

--- Ders02_Express/index.js
+++ Ders02_Express/index.js
@@ -1,23 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const userRouter = require('./routes/user')
 app.use('/libs', express.static(path.join(__dirname, 'node_modules'))) // /libs/bootstrap/dist/css/bootstrap.min.css
 app.use('/static', express.static(path.join(__dirname, 'public'))) // public klasörünüde dışarıya açtık
-
-app.use('/blogs/:blogid/', (req, res) => {
-  console.log(req.params.blogid)
-  console.log(req.params.username)
-  res.sendFile(path.join(__dirname, 'views/users', 'blog-details.html'))
-})
-
-app.use('/blogs', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/users', 'blogs.html'))
-})
-
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/users', 'index.html'))
-})
+app.use(userRouter)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
